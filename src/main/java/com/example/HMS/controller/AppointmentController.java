01d9663b04c62f222b01357b337a864d8c596e91
Debug-akash999 <akasharavindh02@gmail.com>
2026-02-26 19:40:36 +0530

    first commit

--- src/main/java/com/example/HMS/controller/AppointmentController.java
+++ src/main/java/com/example/HMS/controller/AppointmentController.java
@@ -56,4 +56,13 @@ public class AppointmentController {
     public ResponseEntity<Appointment> getById(@PathVariable Long id) {
         return ResponseEntity.ok(serr.getById(id));
     }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        boolean deleted = serr.deleteById(id);
+        if (deleted) {
+            return ResponseEntity.noContent().build(); // 204 No Content
+        } else {
+            return ResponseEntity.notFound().build(); // 404 Not Found
+        }
+    }
 }
